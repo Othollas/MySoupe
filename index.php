49fc9b4dee5eae03585500eab76d2c754cca7d63
o_thollas <o_thollas@hotmail.com>
2024-03-05 17:32:51 +0100

    application style css page d'accueil + creation de la fonction de randomisation d'image

--- index.php
+++ index.php
@@ -4,9 +4,27 @@ $bd = new PDO($dns, 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTIO
 $query = $bd->query('SELECT * FROM soup');
 $soupes = $query->fetchAll(PDO::FETCH_ASSOC);
 include 'template/header.php';
-// var_dump($soupes);
+
 /*foreach($soupes as $soupe){ ?>
    <p><a href='soupe.php?id=<?= $soupe['id'] ?>'><?= $soupe['name'] ?> </a><p><?php }; */
+
+
+function imgRand()
+{
+   $dns = 'mysql:host=localhost;dbname=mysoupe';
+   $bd = new PDO($dns, 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+   $query = $bd->query('SELECT id, picture FROM soup');
+   $soupes = $query->fetchAll(PDO::FETCH_ASSOC);
+   $random = rand(min: 1,  max: count($soupes));
+   foreach ($soupes as $soup) {
+      if ($random == $soup["id"]) {
+         return $soup["picture"];
+      }
+   }
+}
+
+
+// echo $random;
 
 ?>
 
@@ -14,9 +32,9 @@ include 'template/header.php';
 
 <section id="presentation">
    <div id="imgSlide">
-      <img style="width:40%; height:20%; padding:50px 0; border-radius:10px;" src="https://media.istockphoto.com/id/1197494143/fr/photo/hommes-mangeant-la-soupe-cr%C3%A9meuse-de-citrouille-r%C3%B4tie-v%C3%A9g%C3%A9talienne.jpg?s=1024x1024&w=is&k=20&c=kyKvNUbT_LruWtkaHAwWP90EvSs47rKjvz4-ctgmojY=" alt="">
+      <img src="https://media.istockphoto.com/id/1197494143/fr/photo/hommes-mangeant-la-soupe-cr%C3%A9meuse-de-citrouille-r%C3%B4tie-v%C3%A9g%C3%A9talienne.jpg?s=1024x1024&w=is&k=20&c=kyKvNUbT_LruWtkaHAwWP90EvSs47rKjvz4-ctgmojY=" alt="">
    </div>
-   <div id="presentation">
+   <div class="presentation">
       <h1>RECETTE DE SOUPE</h1>
       <h2>Pour tout les goûts en toutes saisons </h2>
       <p>Quelques phrases pour expliquer le site <br>
@@ -25,27 +43,27 @@ include 'template/header.php';
 </section>
 
 <section style="margin:50px;" id="recette">
-   <div id="recettesSaisons">
+   <div class="recettesSaisons">
       <h2>Les recettes du moment</h2>
       <a href="">Voir les recettes</a>
    </div>
-   <div id="randomRecette">
-      <a href=""><img src="" alt=""></a>
-      <a href=""><img src="" alt=""></a>
-      <a href=""><img src="" alt=""></a>
+   <div class="randomRecette">
+      <a href=""><img src="./img/recipes/<?= imgRand() ?>" alt=""></a>
+      <a href=""><img src="./img/recipes/<?= imgRand() ?>" alt=""></a>
+      <a href=""><img src="./img/recipes/<?= imgRand() ?>" alt=""></a>
    </div>
 </section>
 
 <section style="margin:50px;" id="top4">
    <div id="top4">
-      <h2>Retrouvez les soupes les plus populaires du moment</h2>
+      <h2>Retrouvez les soupes les plus recentes</h2>
    </div>
 
    <div id="imgTop4">
-      <a href=""><img src="" alt=""></a>
-      <a href=""><img src="" alt=""></a>
-      <a href=""><img src="" alt=""></a>
-      <a href=""><img src="" alt=""></a>
+      <a href=""><img src="./img/recipes/<?= imgRand() ?>" alt=""></a>
+      <a href=""><img src="./img/recipes/<?= imgRand() ?>" alt=""></a>
+      <a href=""><img src="./img/recipes/<?= imgRand() ?>" alt=""></a>
+      <a href=""><img src="./img/recipes/<?= imgRand() ?>" alt=""></a>
    </div>
 </section>
 
--- template/footer.php
+++ template/footer.php
@@ -1,7 +1,32 @@
 <footer>
-    c'est le footer
+    <section id="titreReseaux">
+        <div>
+            <h1><span>My</span>Soupe</h1>
+        </div>
+        <div id="reseaux">
+            <a href=""><img src="./img/facebook.png" alt=""></a>
+            <a href=""><img src="./img/twitter.png" alt=""></a>
+            <a href=""><img src="./img/instagram.png" alt=""></a>
+            <a href=""><img src="./img/pinterest.png" alt=""></a>
+        </div>
+    </section>
+    <section id="mention">
+        <div id="annonceur">
+            <a href=""><img src="" alt="">annonceur - Publicité</a>
+            <a href="">Partenaire</a>
+            <a href="">Presse</a>
+        </div>
+        <div id="mentionsLegales">
+            <a href="">Nous Contacter</a>
+            <a href="">Mentions légales</a>
+            <a href="">Charte de confidentialité</a>
+            <a href="">Cookies</a>
+        </div>
+    </section>
 </footer>
 
-</main> 
+</main>
 </body>
+<script src=""></script>
+
 </html>
